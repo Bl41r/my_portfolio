a5d8e4a38a9d2ccc6236edf01083f8c9435c0cc1
bl41r <dbsmith.dbs83@gmail.com>
2017-02-27 13:41:55 -0800

    blog view hooked up

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_view(request):
+    """The home view (blog)."""
+    context = {
+        'page_title': 'Home',
+    }
+    return render(request, 'blog/index.html', context)
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aboutme',
+    'blog',
+    'portfolio',
+    'projects',
 ]
 
 MIDDLEWARE = [
