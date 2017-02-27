ac825c74d4323e950371264982b5e609ec2873a6
bl41r <dbsmith.dbs83@gmail.com>
2017-02-27 14:34:00 -0800

    blogarticle model, postgres hooked up

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import url
 from .views import home_view
 
 urlpatterns = [
-    url(
-        r'^$', home_view, name='home_view'
-    ),
+    url(r'^$', home_view, name='home_view'),
 ]
