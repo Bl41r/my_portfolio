9fa68455c4f801b595b06fb6a3d4a29dcecb8f54
bl41r <dbsmith.dbs83@gmail.com>
2017-03-02 15:16:09 -0800

    linefix

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -15,6 +15,8 @@ class ProjectView(TemplateView):
         """View for the home page."""
         context = super(ProjectView, self).get_context_data(**kwargs)
         context['page_title'] = 'My Projects'
-        context['projects'] = Project.objects.filter(author__user__username='david').all().order_by('-date_added')
+        context['projects'] = Project.objects. \
+            filter(author__user__username='david'). \
+            all().order_by('-date_added')
 
         return context
